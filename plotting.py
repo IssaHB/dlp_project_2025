90739de6436693d9942896d34c41ded75f05ac9c
IssaHB <144515512+IssaHB@users.noreply.github.com>
2025-04-04 02:53:35 +0200

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,3 +1,24 @@
+# plotting.py
+#
+# Copyright 2022 Javier Duarte
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# MODIFIED BY: Isa Chousein Basia
+# DATE: 2025-04-04
+# CHANGES: Added grid lines and labels to the plot, as well as a parameter
+# to rotate the x_ticks in the confusion matrix by a desired angle.
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
